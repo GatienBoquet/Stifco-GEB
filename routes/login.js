fc2122e512e223927f608893ae187b34788ac249
Gatien Boquet <gatien.boquet@gmail.com>
2015-03-27 19:05:29 +0100

    up

--- routes/login.js
+++
@@ -1,7 +0,0 @@
-exports.login = function(req, res){
-	res.render('login');
-}
-
-exports.inscription = function(req, res){
-	res.render('inscription');
-};
--- routes/offre.js
+++
@@ -1,3 +0,0 @@
-exports.crud = function(req, res){
-	res.render('offre');
-};
