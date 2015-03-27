5ab351316efac68fbcc9f55840a80103fd8de767
Gatien Boquet <gatien.boquet@gmail.com>
2015-03-27 19:32:58 +0100

    Login
    Still work in progress

--- routes/login.js
+++ routes/login.js
@@ -1,7 +1,30 @@
-exports.login = function(req, res){
-	res.render('login');
-}
-
-exports.inscription = function(req, res){
-	res.render('inscription');
+// Contrôleur login 
+module.exports = function(app, io){
+	app.get('/login', function(req, res){
+		res.render('login');
+		io.sockets.on('connection', function(socket){
+			socket.on('login', function(login){
+				
+				console.log(login);
+				
+				var IDnavigo = login.idnavigo;
+				var password = login.mdp;
+				mysqlClient.query("SELECT numCarteNavigo,motDePasse FROM utitlisateur WHERE numCarteNavigo='" + IDnavigo + "' AND motDePasse='" + password + "';",function(error,res){
+					if(error){
+						console.log("nope");
+						console.log("Login : " + IDnavigo);
+						console.log("Password : " + password);
+					}else{
+						req.session.id  = login.idnavigo;
+						console.log("nice");
+					
+					}
+				
+				
+				});
+				
+				
+			});
+		});
+	});
 };
