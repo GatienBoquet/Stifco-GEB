016d9c04701ae4f10c18d61d808364308c93b648
Gatien Boquet <gatien.boquet@gmail.com>
2015-03-27 19:56:53 +0100

    Fix + module

--- app.js
+++ app.js
@@ -9,9 +9,9 @@ var server = app.listen(8080);
 var io = require('socket.io').listen(server);
 
 require('./routes')(app, io);
-require('./routes/login');
+require('./routes/login')(app, io);
 require('./routes/demande')(app, io);
-require('./routes/offre');
+require('./routes/offre')(app, io);
 
 
 // -- Configuration --
--- routes/login.js
+++ routes/login.js
@@ -22,8 +22,6 @@ module.exports = function(app, io){
 				
 				
 				});
-				
-				
 			});
 		});
 	});
