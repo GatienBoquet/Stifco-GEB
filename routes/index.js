4c9471a555018983f789510dbc9280470ece8513
Gatien Boquet <gatien.boquet@gmail.com>
2015-03-28 15:01:01 +0100

    login

--- routes/index.js
+++ routes/index.js
@@ -1,12 +1,12 @@
 // Contrôleur index
 module.exports = function(app, io){
 	app.get('/', function(req, res){
-		console.log(req.session.idnavigo);
-		if(req.session.idnavigo  == 'undefined'){
-			res.render('index', {idnavigo: "0"});
+		console.log("ID navigo = " + req.session.idnavigo);
+		if(typeof(req.session.idnavigo)  == 'undefined'){
+			res.render('index', {idnavigo: '0', prenom: "Inviter"});
 		}
 		else{
-			res.render('index', {idnavigo: req.session.idnavigo});
+			res.render('index', {idnavigo: req.session.idnavigo, prenom :req.session.prenomuser});
 		}
 	});
 };
--- routes/login.js
+++ routes/login.js
@@ -7,21 +7,24 @@ module.exports = function(app, io){
 				console.log("LOGIN WORK IN PROGRESS");
 				var IDnavigo = login.idnavigo;
 				var password = login.mdp;
-				mysqlClient.query("SELECT numCarteNavigo,motDePasse FROM utitlisateur WHERE numCarteNavigo='" + IDnavigo + "' AND motDePasse='" + password + "';",function(error,res){
+				mysqlClient.query("SELECT numCarteNavigo,motDePasse,id,nom,prenom FROM utitlisateur WHERE numCarteNavigo='" + IDnavigo + "' AND motDePasse='" + password + "';",function(error, rows, res){
 					if(error){
 						console.log("Mauvais password ou login");
 						console.log("Login : " + IDnavigo);
 						console.log("Password : " + password);
 					}else{
 						req.session.idnavigo = IDnavigo;
+						req.session.iduser = rows[0].id;
+						req.session.nomuser = rows[0].nom;
+						req.session.prenomuser = rows[0].prenom;
 						req.session.save();
-						console.log("You're now logged in.");
-						console.log(req.session.idnavigo);
-					
+						console.log("Session OK");
+						console.log("Tu es l'id navigo = " + req.session.idnavigo);
+						console.log("ID user : " + req.session.iduser);
 					}
-				
-				
 				});
+				console.log("REDIRECTION NOW");
+				res.redirect('/boo');
 			});
 		});
 	});
